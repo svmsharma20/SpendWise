7a18982eb2accf5d298e1dbfc153b2664e6f18c4
shivam <shivamsharmasv@gmail.com>
2023-04-02 18:57:29 +0530

    Optimized import

--- src/main/java/com/bitbyteclub/SpendWise/entities/UserAuthority.java
+++ src/main/java/com/bitbyteclub/SpendWise/entities/UserAuthority.java
@@ -1,7 +1,7 @@
 package com.bitbyteclub.SpendWise.entities;
 
-import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
